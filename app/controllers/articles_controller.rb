3ac7781e9eed4d8e28bbd3b483abdec643b65b7a
Rupesh Baberwal <rupesh.baberwal50@gmail.com>
2021-04-28 19:46:37 +0530

    initial project commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+   def index
+    @articles = Article.all
+  end
+
+   def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+     
+     @article = Article.new(params.require(:article).permit(:title, :text))
+
+     if @article.save
+     redirect_to @article
+   else 
+    render 'new'
+   end
+  end
+
+   private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+ end
